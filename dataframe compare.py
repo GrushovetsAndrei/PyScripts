1abd032876a5c3723770812907cef91b68725eff
Grushovec Andrei <grand_77@mail.ru>
2025-10-17 09:27:56 +0300

    Update dataframe compare.py
    Добавлен вывод совпадений

--- dataframe compare.py
+++ dataframe compare.py
@@ -4,44 +4,11 @@
   False  - количество уникальных значений 
 '''
 
-fn1 = 'd:\\WA\\327 Сливки\\Master Gourmet.csv'
-fn2 = 'd:\\WA\\327 Сливки\\AG5ATD Martini Plus Sfoglia 1413 кодов.csv'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fn1 = 't:\\Складской КОМПЛЕКС\\GrAnd\\test.txt'
+fn2 = 't:\\Складской КОМПЛЕКС\\GrAnd\\test2.txt'
 
 import pandas as pd
-import numpy as np
 from io import StringIO
-
 
 
 pd.options.mode.copy_on_write = True
@@ -50,14 +17,21 @@ with open(fn1, encoding='UTF-8') as file:
     csvString = file.readlines()
 csvString = ''.join(csvString)
 csvString = StringIO(csvString)
+#С заголовками
+#df1 = pd.read_csv(csvString, sep='\t', encoding='utf-8', dtype=object)
+#Без заголовков
 df1 = pd.read_csv(csvString, sep='\t', encoding='utf-8', dtype=object, header=None)
-
 with open(fn2, encoding='UTF-8') as file:
     csvString = file.readlines()
 csvString = ''.join(csvString)
 csvString = StringIO(csvString)
 df2 = pd.read_csv(csvString, sep='\t', encoding='utf-8', dtype=object, header=None)
-df1[1] = df2[0]
-df1[2] = df1[1].isin(df1[0])
-#df1[2] = df1[2] * 1
-print(df1[2].value_counts())
+
+#Добавить последним столбцом данные для сравнения (2 - номер столбца в df2 для сравнения)
+df1[len(df1.columns.to_list())] = df2[2]
+#Сравнение данных в столбцах и сохрание резхзультата сравнения в столбце Compare
+df1['Compare'] = df1[2].isin(df1[len(df1.columns.to_list())-1])
+
+print(df1['Compare'].value_counts())
+print(f'Совпадающие коды: {df1['Compare'].value_counts()[True]}')
+print(list(df1[df1['Compare']==True][2]))
